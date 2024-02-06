5d757ddc51db29531e98f7fd89ad8f7351914448
K Thomas <sven45@gmail.com>
2024-02-06 18:10:29 +0100

    Modficiation connection DB

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ mongoose.connect('mongodb+srv://KTOC:AzertyOC@clusteroc.eey0xu4.mongodb.net/?ret
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// // Connexion à la base de données
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://KTOC:AzertyOC@clusteroc.eey0xu4.mongodb.net/?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("KTOC").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 // CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
